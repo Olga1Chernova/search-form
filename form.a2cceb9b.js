8e68b7c1ff934e76eaa11ff8a70d4062a6dd83ae
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2022-12-15 22:42:05 +0000

    Deploying to gh-pages from @ Olga1Chernova/search-form@e438a74eef540e13e8690deb7b4409c969cd7152 🚀

--- form.a2cceb9b.js
+++
@@ -1,2 +0,0 @@
-const t={startButton:document.querySelector("[data-start]"),stopButton:document.querySelector("[data-stop]"),body:document.querySelector("body")};let o=null;function e(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.stopButton.disabled=!0,t.startButton.addEventListener("click",(()=>{o=setInterval(e,1e3),t.startButton.disabled=!0,t.stopButton.disabled=!1})),t.stopButton.addEventListener("click",(()=>{clearInterval(o),t.startButton.disabled=!1,t.stopButton.disabled=!0}));
-//# sourceMappingURL=form.a2cceb9b.js.map
